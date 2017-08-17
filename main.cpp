ecf94fdb70ee0abea128bcafffe83c1e8f938cba
Alexandre Araujo <alexandre.araujo@absoluta.net>
2017-08-17 18:10:31 -0300

    code changed

--- main.cpp
+++ main.cpp
@@ -1,22 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stack>
 
 int main(int argc, char* argv[])
 {
     std::ifstream fileStream("../Paths_D.txt");
     std::string lineBuffer;
     std::string ratio;
+    int x, y;
 
     std::getline(fileStream, lineBuffer);
     ratio = lineBuffer;
 
+    std::stack<char> brackets;
+
+    std::string number;
+    int converted;
+
     while (fileStream && std::getline(fileStream, lineBuffer)) {
         for (char &c : lineBuffer) {
-            std::cout << c;
-        }
+            if (c == '(') {
+                brackets.push(c);
+                continue;
+            }
 
-        std::cout << '\n';
+            if (brackets.empty()) {
+                continue;
+            }
+
+            if (c == ',') {
+                converted = std::stoi(number);
+                std::cout << converted << '\n';
+
+                if (1 /* first number */) {
+
+                } else if (2 /* second number */) {
+
+                } else  /* third number */ {
+
+                }
+
+                number = "";
+                continue;
+            }
+            number += c;
+
+            if (c == ')') {
+                brackets.pop();
+                continue;
+            }
+
+            if (c == '\t') {
+                // maybe it's not necessary
+                continue;
+            }
+        }
     }
 
     return 0;
