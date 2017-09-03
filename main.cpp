d4e345ae6a780807fee516575598819360da60b1
Alexandre Araujo <alexandreh.araujo@gmail.com>
2017-09-03 10:26:03 -0300

    fix file reading and indentation

--- main.cpp
+++ main.cpp
@@ -1,27 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <limits>
 #include <OpenGL/gl.h>
 #include <GLUT/glut.h>
 
 using namespace std;
 
 class Coordinate {
- public:
-  Coordinate() {
-    this->x = 0;
-    this->y = 0;
-  }
+  public:
+    Coordinate() {
+      this->x = 0;
+      this->y = 0;
+    }
 
-  double x;
-  double y;
+    double x;
+    double y;
 };
 
-Coordinate *readPlayerCoordinates(const string &lineBuffer, int duration);
+class Window {
+  public:
+    Window() {
+        this->minX = numeric_limits<double>::max();
+        this->minY = numeric_limits<double>::max();
+        this->maxX = numeric_limits<double>::min();
+        this->maxY = numeric_limits<double>::min();
+    }
+
+    double minX;
+    double minY;
+    double maxX;
+    double maxY;
+};
+
+Coordinate *readPlayerCoordinates(const string &lineBuffer, int duration, Window* window);
 void drawCallback(void);
 void keyboardCallback(unsigned char key, int x, int y);
-void initWindowSize(void);
+void initWindowSize(double left, double right, double bottom, double top);
+void printMatrix(int biggestDuration, int playersCount, Coordinate *const *matrix);
 
 /**
  * Application startup
@@ -31,136 +46,145 @@ void initWindowSize(void);
  * @return
  */
 int main(int argc, char *argv[]) {
+    ifstream fileStream;
+    string lineBuffer;
+    string ratio;
+    int biggestDuration = 0, curr = 0, playersCount = 0;
 
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_3_2_CORE_PROFILE);
-  glutInitWindowSize(500,500);
-  glutCreateWindow("Desenho de um triângulo em 2D");
+    fileStream.open("../Paths_D.txt");
 
-  // Registra a função callback de redesenho da janela de visualização
-  glutDisplayFunc(drawCallback);
-  // Registra a função callback para tratamento das teclas ASCII
-  glutKeyboardFunc(keyboardCallback);
-  // Chama a função responsável por fazer as inicializações
-  initWindowSize();
-  // Inicia o processamento e aguarda interações do usuário
-  glutMainLoop();
+    // reading ratio
+    getline(fileStream, lineBuffer);
+    ratio = stod(lineBuffer.substr(1, lineBuffer.length() - 2));
 
-  ifstream fileStream;
-  string lineBuffer;
-  string ratio;
-  int biggestDuration = 0, curr = 0, playersCount = 0;
+    // count number of actors and longest duration
+    while (fileStream >> curr) {
+        if (curr > biggestDuration) {
+            biggestDuration = curr;
+        }
 
-  fileStream.open("../Paths_D.txt");
-
-  // reading ratio
-  getline(fileStream, lineBuffer);
-  ratio = stod(lineBuffer.substr(1, lineBuffer.length() - 2));
-
-  // count number of actors and longest duration
-  while (fileStream >> curr) {
-    if (curr > biggestDuration) {
-      biggestDuration = curr;
+        playersCount++;
+        fileStream.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    playersCount++;
+    // reset file reading
+    fileStream.close();
+    fileStream.open("../Paths_D.txt");
+    // ignore first line
     fileStream.ignore(numeric_limits<streamsize>::max(), '\n');
-  }
 
-  // reset file reading
-  fileStream.close();
-  fileStream.open("../Paths_D.txt");
-  // ignore first line
-  fileStream.ignore(numeric_limits<streamsize>::max(), '\n');
+    auto ** matrix = new Coordinate*[playersCount];
+    auto * window = new Window;
 
-  auto ** matrix = new Coordinate*[playersCount];
-
-  int i=0;
-  while (fileStream && getline(fileStream, lineBuffer)) {
-    matrix[i++] = readPlayerCoordinates(lineBuffer, biggestDuration);
-  }
-
-  for (int i = 0; i < playersCount; i++) {
-    for (int j = 0; j < biggestDuration; j++) {
-        auto point = matrix[i][j];
-        cout << '(';
-        cout << point.x;
-        cout << ',';
-        cout << point.y;
-        cout << ')';
+    int i=0;
+    while (fileStream && getline(fileStream, lineBuffer)) {
+        matrix[i++] = readPlayerCoordinates(lineBuffer, biggestDuration, window);
     }
+    //printMatrix(biggestDuration, playersCount, matrix);
 
-    cout << endl;
-  }
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    //glutInitWindowSize(static_cast<int>(maxX-minX), static_cast<int>(maxY-minY));
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Análise de movimentação");
+    // Registra a função callback de redesenho da janela de visualização
+    glutDisplayFunc(drawCallback);
+    // Registra a função callback para tratamento das teclas ASCII
+    glutKeyboardFunc(keyboardCallback);
+    // Chama a função responsável por fazer as inicializações
+    //initWindowSize(minX, maxX, minY, maxY);
+    initWindowSize(0, 1500, 0, 1500);
+    // Inicia o processamento e aguarda interações do usuário
+    glutMainLoop();
 
-  return 0;
+    return 0;
 }
 
 /**
- *
+ * @todo: pass declared object instead of its size
  * @param lineBuffer
  */
-Coordinate *readPlayerCoordinates(const string &lineBuffer, int duration) {
-  auto * playersCoordinate = new Coordinate[duration];
-  string storage, limit;
-  int number = 0, i = 0;
+Coordinate *readPlayerCoordinates(const string &lineBuffer, int duration, Window* window) {
+    auto * playersCoordinate = new Coordinate[duration];
+    Coordinate *point = nullptr;
+    string storage, limit;
+    int number = 0, i = 0;
 
-  istringstream ss(lineBuffer);
-  ss >> limit;
+    istringstream ss(lineBuffer);
+    ss >> limit;
 
-  while (! ss.eof()) {
-    auto c = (char) ss.peek();
-    if (c == '(' || c == ')' || c == ',' || c == '\t') {
-      ss.ignore();
-      continue;
+    while (! ss.eof()) {
+        auto c = (char) ss.peek();
+        if (c == '(' || c == ')' || c == ',' || c == '\t') {
+            ss.ignore();
+            continue;
+        }
+
+        ss >> number;
+        int index = i/3;
+        if (duration == index) {
+            break;
+        }
+        if (i%3 == 0) {
+            point = new Coordinate;
+            point->x = number;
+        } else if (i%3 == 1) {
+            point->y = number;
+        } else {
+            playersCoordinate[number] = *point;
+
+            if (point->x > window->maxX) {
+                window->maxX = point->x;
+            }
+            if (point->x < window->minX) {
+                window->minX = point->x;
+            }
+            if (point->y > window->maxY) {
+                window->maxY = point->y;
+            }
+            if (point->y < window->minY) {
+                window->minY = point->y;
+            }
+        }
+
+        i++;
     }
 
-    ss >> number;
-    int index = i/3;
-    if (duration == index) {
-      break;
-    }
-    if (i%3 == 0) {
-      playersCoordinate[index].x = number;
-    } else if (i%3 == 1) {
-      playersCoordinate[index].y = number;
-    }
-    i++;
-  }
-
-  return playersCoordinate;
+    return playersCoordinate;
 }
 
 /**
  * @param void
  */
 void drawCallback(void) {
-  // Limpa a janela de visualização com a cor branca
-  glClearColor(1,1,1,0);
-  glClear(GL_COLOR_BUFFER_BIT);
+    // Limpa a janela de visualização com a cor branca
+    glClearColor(1,1,1,0);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-  // Define a cor de desenho: azul
-  glColor3f(0,0,1);
+    // Define a cor de desenho: azul
+    glColor3f(0,0,1);
 
-  // Desenha um triângulo no centro da janela
-  glBegin(GL_TRIANGLES);
-  glVertex3f(-0.5,-0.5,0);
-  glVertex3f( 0.0, 0.5,0);
-  glVertex3f( 0.5,-0.5,0);
-  glEnd();
+    // Desenha um triângulo no centro da janela
+    glBegin(GL_LINES);
+    glVertex2f(500, 500);
+    glVertex2f(750, 500);
+    glEnd();
 
-  //Executa os comandos OpenGL
-  glFlush();
+    //Executa os comandos OpenGL
+    glFlush();
 }
 
 /**
- * @param void
+ *
+ * @param left
+ * @param right
+ * @param bottom
+ * @param top
  */
-void initWindowSize(void) {
-  // Define a janela de visualização 2D
-  glMatrixMode(GL_PROJECTION);
-  gluOrtho2D(-1.0,1.0,-1.0,1.0);
-  glMatrixMode(GL_MODELVIEW);
+void initWindowSize(double left, double right, double bottom, double top) {
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(left, right, bottom, top);
+    glMatrixMode(GL_MODELVIEW);
 }
 
 /**
@@ -170,7 +194,30 @@ void initWindowSize(void) {
  * @param y
  */
 void keyboardCallback(unsigned char key, int x, int y) {
-  if (key == 27) {
-    exit(0);
-  }
+    if (key == 27) {
+        exit(0);
+    }
+}
+
+/**
+ *
+ * @param biggestDuration
+ * @param playersCount
+ * @param matrix
+ */
+void printMatrix(int biggestDuration, int playersCount, Coordinate *const *matrix) {
+    for (int i = 0; i < playersCount; i++) {
+        for (int j = 0; j < biggestDuration; j++) {
+            auto point = matrix[i][j];
+            cout << '(';
+            cout << point.x;
+            cout << ',';
+            cout << point.y;
+            cout << ',';
+            cout << j;
+            cout << ')';
+        }
+
+        cout << endl;
+    }
 }
